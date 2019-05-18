c44a617602c65d19dab365e889e0b04c5fa1f41b
lanrex52 <lanrex52@gmail.com>
2019-05-17 23:11:12 -0600

    project

---
+++ EmployeeManagement/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace EmployeeManagement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.dgvemployees = new System.Windows.Forms.DataGridView();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FirstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sex = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Age = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Salary = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Address = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.rbtMale = new System.Windows.Forms.RadioButton();
+            this.rbtFemale = new System.Windows.Forms.RadioButton();
+            this.cmbAge = new System.Windows.Forms.ComboBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvemployees)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FirstName";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(111, 81);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(125, 20);
+            this.txtFirstName.TabIndex = 1;
+            this.txtFirstName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtLastname
+            // 
+            this.txtLastname.Location = new System.Drawing.Point(111, 138);
+            this.txtLastname.Name = "txtLastname";
+            this.txtLastname.Size = new System.Drawing.Size(125, 20);
+            this.txtLastname.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Lastname";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Sex";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 261);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Age";
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Location = new System.Drawing.Point(111, 310);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(125, 20);
+            this.txtSalary.TabIndex = 9;
+            this.txtSalary.TextChanged += new System.EventHandler(this.txtSalary_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(35, 310);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Salary";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(111, 352);
+            this.txtAddress.Multiline = true;
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(163, 67);
+            this.txtAddress.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(35, 352);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Address";
+            // 
+            // btnsave
+            // 
+            this.btnsave.Location = new System.Drawing.Point(64, 436);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(62, 23);
+            this.btnsave.TabIndex = 12;
+            this.btnsave.Text = "Save";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // btndelete
+            // 
+            this.btndelete.Location = new System.Drawing.Point(144, 436);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(75, 23);
+            this.btndelete.TabIndex = 13;
+            this.btndelete.Text = "Delete";
+            this.btndelete.UseVisualStyleBackColor = true;
+            this.btndelete.Click += new System.EventHandler(this.btndelete_Click);
+            // 
+            // dgvemployees
+            // 
+            this.dgvemployees.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvemployees.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.FirstName,
+            this.LastName,
+            this.Sex,
+            this.Age,
+            this.Salary,
+            this.Address});
+            this.dgvemployees.Location = new System.Drawing.Point(439, 46);
+            this.dgvemployees.Name = "dgvemployees";
+            this.dgvemployees.Size = new System.Drawing.Size(637, 295);
+            this.dgvemployees.TabIndex = 14;
+            this.dgvemployees.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvemployees_CellContentClick);
+            this.dgvemployees.DoubleClick += new System.EventHandler(this.dgvemployees_DoubleClick);
+            // 
+            // Id
+            // 
+            this.Id.DataPropertyName = "Id";
+            this.Id.HeaderText = "Id";
+            this.Id.Name = "Id";
+            this.Id.Visible = false;
+            // 
+            // FirstName
+            // 
+            this.FirstName.DataPropertyName = "FirstName";
+            this.FirstName.HeaderText = "FirstName";
+            this.FirstName.Name = "FirstName";
+            // 
+            // LastName
+            // 
+            this.LastName.DataPropertyName = "LastName";
+            this.LastName.HeaderText = "LastName";
+            this.LastName.Name = "LastName";
+            // 
+            // Sex
+            // 
+            this.Sex.DataPropertyName = "Sex";
+            this.Sex.HeaderText = "Sex";
+            this.Sex.Name = "Sex";
+            // 
+            // Age
+            // 
+            this.Age.DataPropertyName = "Age";
+            this.Age.HeaderText = "Age";
+            this.Age.Name = "Age";
+            // 
+            // Salary
+            // 
+            this.Salary.DataPropertyName = "Salary";
+            this.Salary.HeaderText = "Salary";
+            this.Salary.Name = "Salary";
+            // 
+            // Address
+            // 
+            this.Address.DataPropertyName = "Address";
+            this.Address.HeaderText = "Address";
+            this.Address.Name = "Address";
+            // 
+            // rbtMale
+            // 
+            this.rbtMale.AutoSize = true;
+            this.rbtMale.Location = new System.Drawing.Point(102, 200);
+            this.rbtMale.Name = "rbtMale";
+            this.rbtMale.Size = new System.Drawing.Size(48, 17);
+            this.rbtMale.TabIndex = 15;
+            this.rbtMale.TabStop = true;
+            this.rbtMale.Text = "Male";
+            this.rbtMale.UseVisualStyleBackColor = true;
+            // 
+            // rbtFemale
+            // 
+            this.rbtFemale.AutoSize = true;
+            this.rbtFemale.Location = new System.Drawing.Point(191, 200);
+            this.rbtFemale.Name = "rbtFemale";
+            this.rbtFemale.Size = new System.Drawing.Size(59, 17);
+            this.rbtFemale.TabIndex = 16;
+            this.rbtFemale.TabStop = true;
+            this.rbtFemale.Text = "Female";
+            this.rbtFemale.UseVisualStyleBackColor = true;
+            // 
+            // cmbAge
+            // 
+            this.cmbAge.FormattingEnabled = true;
+            this.cmbAge.Location = new System.Drawing.Point(111, 253);
+            this.cmbAge.Name = "cmbAge";
+            this.cmbAge.Size = new System.Drawing.Size(121, 21);
+            this.cmbAge.TabIndex = 17;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(253, 436);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 18;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1098, 490);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.cmbAge);
+            this.Controls.Add(this.rbtFemale);
+            this.Controls.Add(this.rbtMale);
+            this.Controls.Add(this.dgvemployees);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtLastname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvemployees)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.DataGridView dgvemployees;
+        private System.Windows.Forms.RadioButton rbtMale;
+        private System.Windows.Forms.RadioButton rbtFemale;
+        private System.Windows.Forms.ComboBox cmbAge;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FirstName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LastName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sex;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Age;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Salary;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Address;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ EmployeeManagement/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmployeeManagement
+{
+    public partial class Form1 : Form
+    {
+        Employee empModel = new Employee();
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+            empModel.FirstName = txtFirstName.Text;
+            empModel.LastName = txtLastname.Text;
+            if (rbtMale.Checked)
+            {
+                empModel.Sex = rbtMale.Text;
+            }
+            else if (rbtMale.Checked)
+            {
+                empModel.Sex = rbtFemale.Text;
+            }
+            empModel.Age = int.Parse(cmbAge.Text);
+            empModel.Salary = decimal.Parse( txtSalary.Text);
+            empModel.Address = txtAddress.Text;
+            try
+            {
+                if (btnsave.Text == "Save" )
+                {
+                    using (EmployeeManagementSystemEntities1 db = new EmployeeManagementSystemEntities1())
+                    {
+                        db.Employees.Add(empModel);
+                        db.SaveChanges();
+                        LoadData();
+                        MessageBox.Show("Data Saved Successfully");
+                      
+                        Clear();
+
+                    }
+                   
+                }
+                else if (btnsave.Text == "Update")
+                {
+                    using (EmployeeManagementSystemEntities1 db = new EmployeeManagementSystemEntities1())
+                    {
+                        db.Entry(empModel).State = EntityState.Modified;
+                        db.SaveChanges();
+                        LoadData();
+                        MessageBox.Show("Data Updated Successfully");
+                        Clear();
+                    }
+                    
+                }
+
+               
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Cannot to save info" + ex.Message);
+            }
+
+           
+         
+       
+        }
+
+        private void txtSalary_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvemployees_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Clear();
+            LoadCombobox();
+            LoadData();
+        }
+
+        public void LoadData()
+        {
+            using (EmployeeManagementSystemEntities1 db = new EmployeeManagementSystemEntities1())
+            {
+                dgvemployees.DataSource = db.Employees.ToList();
+            }
+           
+        }
+        public void LoadCombobox()
+        {
+            for (int i = 20; i <= 55; i++)
+            {
+                cmbAge.Items.Add(i);
+            }
+
+        }
+        public void Clear()
+        {
+            txtFirstName.Text = txtLastname.Text = txtSalary.Text = txtAddress.Text = cmbAge.Text = "";
+            rbtMale.Checked = true;
+            btnsave.Text = "Save";
+            btndelete.Enabled = false;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void dgvemployees_DoubleClick(object sender, EventArgs e)
+        {
+            using (EmployeeManagementSystemEntities1 db = new EmployeeManagementSystemEntities1())
+            {
+                var employeeid = dgvemployees.CurrentRow.Cells["Id"].Value;
+                var theRestOfTheEmployeeData = db.Employees.Find(employeeid);
+                txtFirstName.Text = theRestOfTheEmployeeData.FirstName;
+                txtLastname.Text = theRestOfTheEmployeeData.LastName;
+                if (theRestOfTheEmployeeData.Sex == "Male")
+                {
+                    rbtMale.Checked = true;
+                }
+                else if (theRestOfTheEmployeeData.Sex == "Female")
+                {
+                    rbtFemale.Checked = true;
+                }
+
+                cmbAge.Text = theRestOfTheEmployeeData.Age.ToString();
+                txtSalary.Text = theRestOfTheEmployeeData.Salary.ToString();
+                txtAddress.Text = theRestOfTheEmployeeData.Address;
+
+                //id to be used to know which record to update
+                empModel.id = theRestOfTheEmployeeData.id;
+            }
+          
+            
+            btnsave.Text = "Update";
+            btndelete.Enabled = true;
+
+        }
+
+        private void btndelete_Click(object sender, EventArgs e)
+        { 
+            try
+            {
+                if (MessageBox.Show("Are you sure you want to delete","Delete Record", MessageBoxButtons.YesNo, MessageBoxIcon.Warning)== DialogResult.Yes)
+                {
+                    //var employeeid = dgvemployees.CurrentRow.Cells["Id"].Value;
+                    using (EmployeeManagementSystemEntities1 db = new EmployeeManagementSystemEntities1())
+                    {
+                        db.Entry(empModel).State = EntityState.Deleted;
+                        db.SaveChanges();
+                        LoadData();
+                        MessageBox.Show("Data Deleted Successfully");
+
+                        Clear();
+                        //second way
+                        //var employeetodelete = db.Employees.Find(employeeid);
+                        //db.Employees.Remove(employeetodelete);
+                        //db.SaveChanges();
+                        //LoadData();
+                        //MessageBox.Show("Data Deleted Successfully");
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Unable to delete data" + ex.Message);
+            }
+
+        }
+    }
+}
